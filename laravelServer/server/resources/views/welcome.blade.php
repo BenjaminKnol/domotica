60111307c2f5d6ffb08b01f28742fe45dd9c996e
Benjamin Knol <Benjamin_knol@hotmail.com>
2020-12-09 13:34:11 +0100

    made more button and updated links

--- laravelServer/server/resources/views/welcome.blade.php
+++ laravelServer/server/resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/9e8d2c6531.js" crossorigin="anonymous"></script>
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
@@ -29,7 +30,8 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Domotica</h1>
+                    <i class="fas fa-home fa-5x"></i>
+                    <h1 style="font-size: 3em; margin-left: 1em">Domotica</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
@@ -108,32 +110,61 @@
                                 </div>
                             </div>
                         </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                            <div class="flex items-center">
+                                <form method="post" action="{{ route('options') }}">
+                                    <div class="onoffswitch">
+                                        {{ csrf_field() }}
+                                        <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="placeholder5" tabindex="0" onchange="this.form.submit()">
+                                        <label class="onoffswitch-label" for="placeholder5"></label>
+                                    </div>
+                                </form>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Placeholder 5</div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Beschrijving placeholder button 5
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                            <div class="flex items-center">
+                                <form method="post" action="{{ route('options') }}">
+                                    <div class="onoffswitch">
+                                        {{ csrf_field() }}
+                                        <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="placeholder6" tabindex="0" onchange="this.form.submit()">
+                                        <label class="onoffswitch-label" for="placeholder6"></label>
+                                    </div>
+                                </form>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Placeholder 6</div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Beschrijving placeholder button 6
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
-                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
-
-                            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                                Shop
+                            <a href="#" class="ml-1 underline">
+                                <i class="far fa-arrow-alt-circle-right fa-lg"></i> link placeholder
                             </a>
-
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
-                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                            </svg>
-
-                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                            <a href="#" class="ml-1 underline">
+                                <i class="fas fa-book fa-lg"></i> link_placeholder
                             </a>
                         </div>
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        placehold
                     </div>
                 </div>
             </div>
