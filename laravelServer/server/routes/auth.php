e72eb8a2fa2bbaea52456a4fd37a8640d2b5a7cf
Michael Rotteveel <michael_rotteveel@hotmail.nl>
2020-12-09 12:22:52 +0100

    Started on logging page

--- laravelServer/server/routes/auth.php
+++ laravelServer/server/routes/auth.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\LoggingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
@@ -62,3 +63,11 @@ Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store']
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+
+// Logging routes
+
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/logs', [LoggingController::class, 'index']);
+    Route::get('/logs{id}', [LoggingController::class, 'show']);
+});
