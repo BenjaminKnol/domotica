6c46cbac052278f51a96a19e66a6ac6ee83d37c9
NoureddineAJ <n_aitjaa@outlook.com>
2021-01-22 18:55:17 +0100

    Added menu function to main for switch case and keeping main function clean.

--- sockets/main.cpp
+++ sockets/main.cpp
@@ -12,9 +12,26 @@
 #include "Socket_threading.h"
 #include "Json_conversion.h"
 
+#include "Components/Chair.h"
+#include "Components/Bed.h"
+#include "Components/Column.h"
+#include "Components/Door.h"
+#include "Components/Fridge.h"
+#include "Components/TableLamp.h"
+#include "Components/Wall.h"
 
 #define MESSAGE_LENGTH 256
 Json_conversion import_export_json;
+
+// Start Component objects
+Bed bed_led_obj;
+Chair chair_obj;
+Fridge fridge_obj;
+Wall wall_obj;
+TableLamp table_lamp_obj;
+Door door_obj;
+Column column_obj;
+// End Component objects
 
 void menu() {
   switch(import_export_json.get_id()) {
@@ -24,21 +41,21 @@ void menu() {
     case 2: // chair
         chair_obj.chair(import_export_json.get_unique_id(), import_export_json.get_status());
         break;
-    // case 3: // fridge
-    //     fridge_obj.fridge(import_export_json.get_unique_id(), import_export_json.get_status());
-    //     break;
-    // case 4: // wall
-    //     wall_obj.wall(import_export_json.get_unique_id(), import_export_json.get_status());
-    //     break;
-    // case 5: // table lamp
-    //     table_lamp_obj.table_lamp(import_export_json.get_unique_id(), import_export_json.get_status());
-    //     break;
-    // case 6: // door
-    //     door_obj.door(import_export_json.get_unique_id(), import_export_json.get_status());
-    //     break;
-    // case 7: // column
-    //     column_obj.column(import_export_json.get_unique_id(), import_export_json.get_status());
-    //     break;
+    case 3: // fridge
+        fridge_obj.fridge(import_export_json.get_unique_id(), import_export_json.get_status());
+        break;
+    case 4: // wall
+        wall_obj.wall(import_export_json.get_unique_id(), import_export_json.get_status());
+        break;
+    case 5: // table lamp
+        table_lamp_obj.table_lamp(import_export_json.get_unique_id(), import_export_json.get_status());
+        break;
+    case 6: // door
+        door_obj.door(import_export_json.get_unique_id(), import_export_json.get_status());
+        break;
+    case 7: // column
+        column_obj.column(import_export_json.get_unique_id(), import_export_json.get_status());
+        break;
     default:
         cout << "Could not match the appropriate ID.\n";
         // error code here
@@ -50,7 +67,6 @@ int main() {
     Socket_server socket;
     socket.create_socket();     // 1. Create Socket 2. Bind Socket. 3. Listen to Socket
 
-
     while (true) {
       string receive_message, send_message;
       string* message_ptr = &receive_message;
@@ -58,7 +74,10 @@ int main() {
         if (child_socket > 0) {
           std::thread thread_socket(Socket_threading(), message_ptr, MESSAGE_LENGTH, child_socket);
           thread_socket.join();
-          import_export_json.deserializer(receive_message); // Get data from JSON-object.
+          if (!(receive_message.empty())) {
+            import_export_json.deserializer(receive_message); // Get data from JSON-object.
+            menu();
+          }
         } else {
             continue;
         }
