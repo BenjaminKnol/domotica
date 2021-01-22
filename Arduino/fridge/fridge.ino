e2a8166b0e57edad67697b78e03c94cf6db0bf39
HanzGretel <nhutten@protonmail.com>
2021-01-22 15:19:34 +0100

    succesfull testcode that reads the temperature accurately, turns on fan and turns on peltier while temp above 4 celcius

--- Arduino/fridge/fridge.ino
+++ Arduino/fridge/fridge.ino
@@ -19,6 +19,8 @@ unsigned int state=0;
 unsigned counter = 0;
 unsigned int last_input=0;
 
+void activateCooling();
+
 void setup(void) {
   Serial.begin(9600);
   //commented out for testing purpose without needing pi
@@ -93,15 +95,8 @@ void loop() {
   }
   */
   
-  //Set PCA9554 outputs (IO44-IO7)
-  Wire.beginTransmission(0x38); 
-  Wire.write(byte(0x01));            
-  Wire.write(byte(outputs << 4));            
-  Wire.endTransmission(); 
-  Serial.print("Digital out: ");
-  Serial.println(outputs&0x0F);
 
-//  delay(500);
+  delay(1000);
 /* commented out for testing purposes
   Serial.println("TCP connection will be closed now!");
   client.stop();
@@ -114,23 +109,42 @@ void readAnalogSensors() {            //Read analog 10bit inputs 0 from MAX11647
   ai0=ai0<<8;                             //bitshift with 8 because the analog input is 10 bits
   ai0 = ai0|Wire.read();  
   unsigned int ai1 = Wire.read()&0x03;
+  ai1=ai1<<8;                             //bitshift with 8 because the analog input is 10 bits
+  ai1 = ai1|Wire.read();
   Serial.print("analog0 is: ");
   Serial.println(ai0); 
   Serial.println("");
   Serial.print("analog1 is: ");
   Serial.println(ai1); 
   Serial.println("");
-  if (ai0 != 0) // If analog input is not zero
-  {
-    Vo = (analogRead(ai0) + analogRead(ai1)) / 2;            //reads voltage of the analog pin (test if there's actually 2)
-    R2 = R1 * (1023.0 / (float)Vo - 1.0);                 //resistance = known resistor value * (1023.0 / voltage -1)
-    logR2 = log(R2);
-    T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));   //calculates temperature in Kelvin
-    Tc = T - 273.15;                                      //converts temp to celcius
-    Serial.print("Temperature: "); 
-    Serial.print(Tc);
-    Serial.println(" C");
-  }else{
-    Serial.print("No temperature detected? Sumthing is borked");
+  Vo = (ai0 + ai1) / 2;            //reads voltage of the analog pin (test if there's actually 2)
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);                 //resistance = known resistor value * (1023.0 / voltage -1)
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));   //calculates temperature in Kelvin
+  Tc = T - 273.15;                                      //converts temp to celcius
+  Serial.print("Temperature: "); 
+  Serial.print(Tc);
+  Serial.println(" C");
+
+  if(Tc > 4){
+    activateCooling();
+  }else {
+    Serial.print("cooling inactive");
+    digitalWrite(D5,LOW);      //peltier?
   }
+}
+
+void activateCooling(){
+  outputs = 0x01;             //0x1 is de fan
+  digitalWrite(D5,HIGH);      //peltier?
+  Serial.println("cooling active");
+  Serial.print("");
+    //Set PCA9554 outputs (IO44-IO7)
+  Wire.beginTransmission(0x38); 
+  Wire.write(byte(0x01));            
+  Wire.write(byte(outputs << 4));            
+  Wire.endTransmission(); 
+  Serial.print("Digital out: ");
+  Serial.println(outputs&0x0F);
+  
 }
