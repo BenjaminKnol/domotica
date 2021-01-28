2dea002a260f92e55bfb62e9c70b58a3d0581bcf
NoureddineAJ <n_aitjaa@outlook.com>
2021-01-28 15:39:46 +0100

    Start over again

---
+++ socket/Socket_server.h
@@ -0,0 +1,50 @@
+/*
+ * This file  ensures that the Raspberry Pi listen to the
+ * connection from the Wemos D1 mini devices.
+ *
+ * Authors:
+ *    Michael Rotteveel
+ *    Benjamin Thomas Knol
+ *    Vireen Jagram
+ *    Niek Hutten
+ *    Noureddine Ait Jaa
+ *
+ * Used sources: https://www.bogotobogo.com/cplusplus/sockets_server_client.php  ---> Socket explanation.
+ */
+
+#ifndef SOCKET_1_2_SOCKET_SERVER_H
+#define SOCKET_1_2_SOCKET_SERVER_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <thread>
+
+using namespace std;
+
+class Socket_server {
+public:
+    Socket_server(); // Default constructor
+
+    // Do-function
+    void create_socket();
+    void listen_to_client();
+    void send_message(string msg, int sock);
+
+    // Ask-function
+    int read_message(string &buf, int length);
+	  int accept_connection();
+
+private:
+    int server_socket;
+    int client_socket;
+    socklen_t client_address_length;
+    struct sockaddr_in server_address, client_address;
+    int port = 9002;
+};
+#endif //SOCKET_1_2_SOCKET_SERVER_H
---
+++ socket/main.cpp
@@ -0,0 +1,41 @@
+/*
+ *
+ * Authors:
+ *    Michael Rotteveel
+ *    Benjamin Thomas Knol
+ *    Vireen Jagram
+ *    Niek Hutten
+ *    Noureddine Ait Jaa
+ */
+
+#include "Socket_server.h"
+#include "Json_conversion.h"
+
+#define MESSAGE_LENGTH 256
+
+int main() {
+    Socket_server socket;
+    socket.create_socket();     // 1. Create Socket 2. Bind Socket. 3. Listen to Socket
+    Json_conversion import_export_json;
+
+    while (true) {
+        string receive_message, send_message;
+        int child_socket = socket.accept_connection(); // 4. Accept Socket Connection
+        if (child_socket > 0) {
+          socket.read_message(receive_message, MESSAGE_LENGTH);
+          if (!(receive_message.empty())) {
+            cout << "Bericht: " << receive_message << endl;
+            import_export_json.deserializer(receive_message); // Get data from JSON-object.
+          }
+        } else {
+            continue;
+        }
+    }
+    return 0;
+}
+
+// ARCHIVED (test) functions or variables:
+// import_export_json.set_id("Schemerlamp"); // Assigning
+// import_export_json.set_status(1);
+// cout << "MAIN 1: " << json_conv.get_id() << endl;
+// cout << "MAIN 2: " << json_conv.get_status() << endl;
